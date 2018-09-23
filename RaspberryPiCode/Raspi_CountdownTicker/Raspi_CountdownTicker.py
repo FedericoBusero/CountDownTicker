cfd7a462112b874dce61e28621cd83542cd4fa3a
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2018-09-23 19:50:45 +0200

    Update Raspi_CountdownTicker.py

--- RaspberryPiCode/Raspi_CountdownTicker/Raspi_CountdownTicker.py
+++ RaspberryPiCode/Raspi_CountdownTicker/Raspi_CountdownTicker.py
@@ -1,21 +1,24 @@
-from tkinter import *
-from tkinter import ttk
-from tkinter import font
-
 import time
 import datetime
 import serial
 import string
+import platform
+import os
+import pygame
+import sys
 
 global endTime
 global clock_mode
 global deltaTime
 global clockStatus
 
-def quit(*args):
-    ticker_updateStatus("",4)
-    root.destroy()
 
+def debug_print(textstr):
+    # print(textstr)
+    # ser.write(textstr.encode())
+    # ser.write(b'\n')
+    pass
+        
 def pretty_time_delta(seconds):
     sign_string = '-' if seconds < 0 else ''
     seconds = abs(int(seconds))
@@ -25,15 +28,33 @@ def pretty_time_delta(seconds):
     if days > 0:
         return '%s%02dd%02d:%02d:%02d' % (sign_string, days, hours, minutes, seconds)
     elif hours > 0:
-        return '%s%02d:%02d:%02d' % (sign_string, hours, minutes, seconds)
+#        return '%s%02d:%02d:%02d' % (sign_string, hours, minutes, seconds)
+        return '%s%d:%02d:%02d' % (sign_string, hours, minutes, seconds)
     else:
         return '%s%02d:%02d' % (sign_string, minutes, seconds)
+
+def ticker_quit(*args):
+    # debug_print("ticker_quit")
+    ticker_updateStatus("",4)
+    if (platform.system()=="Windows"):
+        quit()
+    else:
+        quit()
+
+def ticker_shutdown(*args):
+    # debug_print("ticker_shutdown")
+    ticker_updateStatus("",4)
+    if (platform.system()=="Windows"):
+        quit()
+    else:
+        os.system("sudo shutdown -h now")
+        quit()
 
 def ticker_StartCountdown(hour, minutes, seconds):
     global endTime
     global deltaTime
     global clock_mode
-    print("ticker_StartCountdown")
+    # debug_print("ticker_StartCountdown")
     clock_mode = 1
     deltaTime = datetime.timedelta(0,(3600*hour) + (60*minutes) + seconds,0,999)
     endTime = datetime.datetime.now() + deltaTime
@@ -42,13 +63,22 @@ def ticker_ResetCountdown(hour, minutes, seconds):
     global endTime
     global deltaTime
     global clock_mode
-    print("ticker_ResetCountdown")
+    # debug_print("ticker_ResetCountdown")
     clock_mode = 2
     deltaTime = datetime.timedelta(0,(3600*hour) + (60*minutes) + seconds,0,999)
     endTime = datetime.datetime.now() + deltaTime
 
+def ticker_test(*args):
+    global endTime
+    global deltaTime
+    global clock_mode
+    # debug_print("ticker_test")
+    clock_mode = 1
+    deltaTime = -datetime.timedelta(0,(3600*9) + (60*59) + 50,0,999)
+    endTime = datetime.datetime.now() + deltaTime
+
 def ticker_pause(*args):
-    print("ticker_pause")
+    # debug_print("ticker_pause")
     global clockStatus
     if clock_mode == 1:
         ticker_updateStatus(clockStatus,2)
@@ -56,7 +86,7 @@ def ticker_pause(*args):
         ticker_updateStatus(clockStatus,1)
 
 def ticker_stop(*args):
-    print("ticker_stop")
+    # debug_print("ticker_stop")
     ticker_updateStatus("",3)
 
 def ticker_updateStatus(tijdstr,newmode):
@@ -66,6 +96,7 @@ def ticker_updateStatus(tijdstr,newmode):
         clockStatus = tijdstr
         clock_mode = newmode
         # print("current mode: ")
+        # print(clock_mode)
         # print(tijdstr)
         ser.write(tijdstr.encode())
         ser.write(b',')
@@ -79,29 +110,35 @@ def resettickerpause(*args):
     ticker_ResetCountdown(0,0,12)
 
 def processSerial():
-    print("processSerial")
+    # debug_print("processSerial")
     line = ser.readline().decode('ascii')
-    print(line)
+    # debug_print(line)
     words = line.split()
-    if words[0] == "Pause":
-        print("Pause")
-        ticker_pause()
-    elif words[0] == "Stop":
-        print("Stop")
-        ticker_stop()
-    elif words[0] == "StartCountdown":
-        print("StartCountdown")
-        ticker_StartCountdown(int(words[1]),int(words[2]),int(words[3]))
-    elif words[0] == "ResetCountdown":
-        print("ResetCountdown")
-        ticker_ResetCountdown(int(words[1]),int(words[2]),int(words[3]))
+    if (len(words)>0):
+        if words[0] == "Pause":
+            # print("Pause")
+            ticker_pause()
+        elif words[0] == "Stop":
+            # print("Stop")
+            ticker_stop()
+        elif words[0] == "Quit":
+            # print("Quit")
+            ticker_shutdown()
+        elif words[0] == "StartCountdown":
+            # print("StartCountdown")
+            if (len(words)>3):
+                ticker_StartCountdown(int(words[1]),int(words[2]),int(words[3]))
+        elif words[0] == "ResetCountdown":
+            # print("ResetCountdown")
+            if (len(words)>3):
+                ticker_ResetCountdown(int(words[1]),int(words[2]),int(words[3]))
 
 def show_time():
     global deltaTime
     global endTime
-    
-    # print("showtime: ")
-    # print(endTime)
+
+    screen.fill(BLACK)
+    # debug_print("showtime");
     if (clock_mode == 1):
         # Get the time remaining until the event
         deltaTime = endTime - datetime.datetime.now()
@@ -110,52 +147,46 @@ def show_time():
         # Get the time remaining until the event
         endTime = datetime.datetime.now() + deltaTime
 
+    if (deltaTime.total_seconds()<(-10*60*60+1)):
+        ticker_updateStatus("",3)
+
     # remove the microseconds part
     ms = deltaTime.microseconds
     deltaTime = deltaTime - datetime.timedelta(microseconds=deltaTime.microseconds)
     # print(pretty_time_delta(deltaTime.total_seconds()))
     if deltaTime.total_seconds()>2*60:
-        lbl.config(foreground="green")
+        colour = GREEN
     else:
-        lbl.config(foreground="red")
+        colour = RED
     if deltaTime.total_seconds()<0:
-        if ms<300000:
-            lbl.config(foreground="black")
+        if ms>0 and ms<300000:
+            colour = BLACK
 
     if (clock_mode == 3):
-        lbl.config(foreground="black")
+        colour = BLACK
     else:
         # Show the time left
         timestr = pretty_time_delta(deltaTime.total_seconds())
         ticker_updateStatus(timestr,clock_mode)
-        txt.set(timestr)
+        debug_print("font render")
+        text = font.render(timestr, True, (colour))
+        x = (screen_width/2 - (text.get_width()) // 2)
+        y = (screen_height/2 - (text.get_height()) // 2)
+        debug_print("screen blit")
+        screen.blit (text, (x,y))
+
+    pygame.display.update()
+    
     #process content on serial port
     moreBytes = ser.inWaiting()
     if moreBytes:
         processSerial()
-        
-    # Trigger the countdown after 1000ms
-    root.after(100, show_time)
 
+if (platform.system()=="Windows"):
+    ser = serial.Serial("COM4",115200)
+else:
+    ser = serial.Serial("/dev/ttyUSB0",115200)
 
-# Use tkinter lib for showing the clock
-root = Tk()
-root.attributes("-fullscreen", True)
-root.configure(background='black')
-root.bind("x", quit)
-root.bind("r", resetticker)
-root.bind("e", resettickerpause)
-root.bind("s", ticker_stop)
-root.bind("p", ticker_pause)
-root.after(1000, show_time)
-
-fnt = font.Font(family='Helvetica', size=200, weight='bold')
-txt = StringVar()
-lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="green", background="black")
-lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-#ser = serial.Serial("/dev/ttyUSB0",115200)
-ser = serial.Serial("COM5",115200)
 ser.close()
 ser.open()
 
@@ -171,7 +202,48 @@ clock_mode = 0
 #ticker_StartCountdown(0,12,0)
 deltaTime = datetime.timedelta(0,0,0,0)
 endTime = datetime.datetime.now()
+debug_print("pygame init")
+pygame.init()
+debug_print("pygame set_mode")
+screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+# screen = pygame.display.set_mode((0,0))
+surface = pygame.display.get_surface() 
+screen_width,screen_height = size = surface.get_width(), surface.get_height()
+pygame.mouse.set_visible(0)
+
+go = True
+
+#Define Colours
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLACK = (0,0,0)
+font = pygame.font.SysFont ("Helvetica", int(screen_width/4.25), True)
+
 ticker_updateStatus("",3)
 
-root.mainloop()
+while go:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            go = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                ticker_quit()
+                
+            elif event.key == pygame.K_r:
+                resetticker()
+
+            elif event.key == pygame.K_e:
+                resettickerpause()
+
+            elif event.key == pygame.K_s:
+                ticker_stop()
+
+            elif event.key == pygame.K_p:
+                ticker_pause()
+
+            elif event.key == pygame.K_t:
+                ticker_test()
+
+    pygame.time.delay(100)
+    show_time()
